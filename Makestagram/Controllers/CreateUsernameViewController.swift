6fdd5acf817427be1492024f16769a6710a53cdb
cindywang222 <cindywang.222@gmail.com>
2018-07-10 12:40:50 -0700

    Add CreateUsernameViewController

---
+++ Makestagram/Controllers/CreateUsernameViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreateUsernameViewController.swift
+//  Makestagram
+//
+//  Created by Cindy Wang on 7/10/18.
+//  Copyright © 2018 cxw. All rights reserved.
+//
+
+import UIKit
+
+class CreateUsernameViewController: UIViewController {
+    
+    //
+    
+}
