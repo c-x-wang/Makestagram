7dee65c2628ef30d84f70b528d8b00624edae8d7
cindywang222 <cindywang.222@gmail.com>
2018-07-11 11:02:49 -0700

    Add view controllers

---
+++ Makestagram/Controllers/FindFriendsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by Cindy Wang on 7/11/18.
+//  Copyright © 2018 cxw. All rights reserved.
+//
+
+import Foundation
---
+++ Makestagram/Controllers/HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  Makestagram
+//
+//  Created by Cindy Wang on 7/11/18.
+//  Copyright © 2018 cxw. All rights reserved.
+//
+
+import Foundation
